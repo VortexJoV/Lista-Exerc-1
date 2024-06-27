95c2597ebfe84149cfc1ab81cbeaf06e29ef070f
João Victor <116215226+VortexJoV@users.noreply.github.com>
2024-06-27 10:28:13 -0300

    Add files via upload

---
+++ Lista_Exercicio1/Lista_Exercicio1/Livros.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lista_Exercicio1
+{
+    public class Livros
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Ano { get; set; }
+
+        public Livros(string titulo, string autor, int ano) 
+        {
+            Titulo = titulo;    
+            Autor = autor;
+            Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            return Titulo
+                + ", "
+                + Autor
+                + ", "
+                + Ano.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Lista_Exercicio1/Lista_Exercicio1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Lista_Exercicio1;
+using System;
+
+List<Livros> livros = new List<Livros>();
+
+Console.WriteLine("Quantos livros irão ser inseridos?");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.Clear();
+    Console.Write("Qual o titulo do livro: ");
+    string titulo = Console.ReadLine();
+
+    Console.Write("Qual o autor do livro: ");
+    string autor = Console.ReadLine();
+
+    Console.Write("Qual o ano do livro: ");
+    int ano = Convert.ToInt32(Console.ReadLine());
+
+    livros.Add(new Livros(titulo, autor, ano));
+    Console.WriteLine();
+}
+
+Console.Clear();
+Console.Write("Digite o nome do Livro que deseja buscar: ");
+string tituloProcurado = Console.ReadLine();
+
+Livros livroEncontrado = livros.Find(livros => livros.Titulo == tituloProcurado);
+
+if (livroEncontrado != null)
+{
+    Console.Clear();
+    Console.WriteLine("Livro encontrado \n\n" + "Titulo: " + livroEncontrado.Titulo);
+    Console.WriteLine("Autor: " + livroEncontrado.Autor);
+    Console.WriteLine("Ano de lançamento: " + livroEncontrado.Ano);
+
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("Livro não encontrado.");
+}
